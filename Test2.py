ef8d581b55d99eb61f8d5fc1cddb1092d66fbbd2
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-29 20:28:03 +0700

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,6 +1,32 @@
+# นาย นวพรรษ นกงาม 6306021620018 Sec 1RA
+
 amount=float(input("Enter amount : "))
 rate=float(input("Enter rate : "))/100
 year=float(input("Enter year : "))
 
 FV=amount*(1+rate)**year
 print("Future value =",round(FV,2))
+
+print("\n")
+print("Future value 1-10 years")
+amount=amount*(1+rate)
+print("Future value 1 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 2 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 3 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 4 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 5 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 6 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 7 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 8 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 9 years =",round(amount,2))
+amount=amount*(1+rate)
+print("Future value 10 years =",round(amount,2))
+
