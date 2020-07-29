3bd3b90af191285ca3795b49767cacdc45531f54
NoTTa1 <68268707+inwnote154@users.noreply.github.com>
2020-07-29 18:46:47 +0700

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,6 +1,5 @@
 amount=float(input("Enter amount : "))
-rate=float(input("Enter rate : "))
-rate=float(rate/100)
+rate=float(input("Enter rate : "))/100
 year=float(input("Enter year : "))
 
 FV=amount*(1+rate)**year
